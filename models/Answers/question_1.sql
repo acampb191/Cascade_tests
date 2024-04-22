864b4c8cc80c24b21654efb1c712578f14773ae5
Alex Campbell <acampb191@gmail.com>
2024-04-21 22:34:46 -0400

    update

--- models/Answers/question_1.sql
+++ models/Answers/question_1.sql
@@ -2,7 +2,7 @@ with
 monthly_sightings as (
     select 
         date_trunc('month', date_witness) as month
-        , city_agent
+        , city_agent 
         , count(*) as sightings
         , max(sightings) over (partition by month) as max_sightings
     from {{ ref('carmen_sightings_all')}}
@@ -10,7 +10,8 @@ monthly_sightings as (
 )
 select 
     month
-    , city_agent
+    , listagg(distinct city_agent, ', ') as city_agent
 from monthly_sightings
 where sightings = max_sightings
+group by all
 order by month desc
